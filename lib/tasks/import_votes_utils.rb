0bcc9e7c476494323d278215625255e28d8a6b85
michaellennox <michaelctlennox@gmail.com>
2016-03-10 16:43:55 +0000

    VoteLine now verifies validity of line

--- lib/tasks/import_votes_utils.rb
+++ lib/tasks/import_votes_utils.rb
@@ -4,11 +4,27 @@ class VoteLine
   end
 
   def valid?
-    return false if vote_line[0] != 'VOTE'
+    return false unless valid_vote_key? && valid_campaign? && valid_validity? && valid_choice?
     true
   end
 
   private
 
   attr_reader :vote_line
+
+  def valid_vote_key?
+    vote_line[0] == 'VOTE'
+  end
+
+  def valid_campaign?
+    vote_line[2][0, 9] == 'Campaign:'
+  end
+
+  def valid_validity?
+    vote_line[3][0, 9] == 'Validity:'
+  end
+
+  def valid_choice?
+    vote_line[4][0, 7] == 'Choice:' && vote_line[4].length > 7
+  end
 end
--- spec/lib/tasks/import_votes_utils_spec.rb
+++ spec/lib/tasks/import_votes_utils_spec.rb
@@ -15,5 +15,29 @@ describe VoteLine do
       invalid_vote_line = VoteLine.new invalid_vote_prefix
       expect(invalid_vote_line.valid?).to be false
     end
+
+    it 'returns false when given an invalid Campaign key' do
+      invalid_campaign_key = %w(VOTE 1168127473 Campagn:ssss_uk_02B Validity:during Choice:Matthew)
+      invalid_vote_line = VoteLine.new invalid_campaign_key
+      expect(invalid_vote_line.valid?).to be false
+    end
+
+    it 'returns false when given an invalid Validity key' do
+      invalid_validity_key = %w(VOTE 1168127473 Campaign:ssss_uk_02B Validty:during Choice:Matthew)
+      invalid_vote_line = VoteLine.new invalid_validity_key
+      expect(invalid_vote_line.valid?).to be false
+    end
+
+    it 'returns false when given an invalid Choice key' do
+      invalid_choice_key = %w(VOTE 1168127473 Campaign:ssss_uk_02B Validity:during Choie:Matthew)
+      invalid_vote_line = VoteLine.new invalid_choice_key
+      expect(invalid_vote_line.valid?).to be false
+    end
+
+    it 'returns false when given an empty Choice value' do
+      invalid_choice_value = %w(VOTE 1168127473 Campaign:ssss_uk_02B Validity:during Choice:)
+      invalid_vote_line = VoteLine.new invalid_choice_value
+      expect(invalid_vote_line.valid?).to be false
+    end
   end
 end
